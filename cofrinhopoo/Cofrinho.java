4bf02ccbe87100292d33627d411618aca78d3d85
Gabriel Silva <gabrielgrsilva@outlook.com.br>
2025-11-18 11:39:55 -0300

    Refactor Remover e Listar Moedas

--- cofrinhopoo/Cofrinho.java
+++ cofrinhopoo/Cofrinho.java
@@ -1,12 +1,15 @@
 package cofrinhopoo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Cofrinho {
-	ArrayList<Moeda> moedas = new ArrayList<>();
+	ArrayList<Moeda> cofrinho = new ArrayList<>();
+	
+	Iterator<Moeda> it = cofrinho.iterator();
 	
 	public Moeda get(int index) {
-		return moedas.get(index);
+		return cofrinho.get(index);
 	}
 	
 	public void adicionar(int escolhaMoeda, int escolhaValor) {
@@ -15,19 +18,19 @@ public class Cofrinho {
 		case 1:
 			switch(escolhaValor) {
 			case 1:
-				moedas.add(new Real(0.10));
+				cofrinho.add(new Real(0.10));
 				System.out.println("Uma moeda de 10 centavos (Real) foi adicionada ao Cofrim!");
 				break;
 			case 2:
-				moedas.add(new Real(0.25));
+				cofrinho.add(new Real(0.25));
 				System.out.println("Uma moeda de 25 centavos (Real) foi adicionada ao Cofrim!");
 				break;
 			case 3:
-				moedas.add(new Real(0.50));
+				cofrinho.add(new Real(0.50));
 				System.out.println("Uma moeda de 50 centavos (Real) foi adicionada ao Cofrim!");
 				break;
 			case 4:
-				moedas.add(new Real(1.00));
+				cofrinho.add(new Real(1.00));
 				System.out.println("Uma moeda de 1 Real foi adicionada ao Cofrim!");
 				break;
 			default:
@@ -36,19 +39,19 @@ public class Cofrinho {
 		case 2:
 			switch(escolhaValor) {
 			case 1:
-				moedas.add(new Dolar(0.10));
+				cofrinho.add(new Dolar(0.10));
 				System.out.println("Uma moeda de 10 centavos (Dólar) foi adicionada ao Cofrim!");
 				break;
 			case 2:
-				moedas.add(new Dolar(0.25));
+				cofrinho.add(new Dolar(0.25));
 				System.out.println("Uma moeda de 25 centavos (Dólar) foi adicionada ao Cofrim!");
 				break;
 			case 3:
-				moedas.add(new Dolar(0.50));
+				cofrinho.add(new Dolar(0.50));
 				System.out.println("Uma moeda de 50 centavos (Dólar) foi adicionada ao Cofrim!");
 				break;
 			case 4:
-				moedas.add(new Dolar(1.00));
+				cofrinho.add(new Dolar(1.00));
 				System.out.println("Uma moeda de 1 Dólar foi adicionada ao Cofrim!");
 				break;
 			default:
@@ -57,19 +60,19 @@ public class Cofrinho {
 		case 3:
 			switch(escolhaValor) {
 			case 1:
-				moedas.add(new Euro(0.10));
+				cofrinho.add(new Euro(0.10));
 				System.out.println("Uma moeda de 10 centavos (Euro) foi adicionada ao Cofrim!");
 				break;
 			case 2:
-				moedas.add(new Euro(0.25));
+				cofrinho.add(new Euro(0.25));
 				System.out.println("Uma moeda de 25 centavos (Euro) foi adicionada ao Cofrim!");
 				break;
 			case 3:
-				moedas.add(new Euro(0.50));
+				cofrinho.add(new Euro(0.50));
 				System.out.println("Uma moeda de 50 centavos (Euro) foi adicionada ao Cofrim!");
 				break;
 			case 4:
-				moedas.add(new Euro(1.00));
+				cofrinho.add(new Euro(1.00));
 				System.out.println("Uma moeda de 1 Euro foi adicionada ao Cofrim!");
 				break;
 			default:
@@ -78,72 +81,12 @@ public class Cofrinho {
 		}
 	}
 	
-	public void remover(int escolhaMoeda, int escolhaValor) {
-		switch(escolhaMoeda) {
-		
-		case 1:
-			switch(escolhaValor) {
-			case 1:
-				moedas.add(new Real(0.10));
-				System.out.println("Uma moeda de 10 centavos (Real) foi removida do Cofrim!");
-				break;
-			case 2:
-				moedas.add(new Real(0.25));
-				System.out.println("Uma moeda de 25 centavos (Real) foi removida do Cofrim!");
-				break;
-			case 3:
-				moedas.add(new Real(0.50));
-				System.out.println("Uma moeda de 50 centavos (Real) foi removida do Cofrim!");
-				break;
-			case 4:
-				moedas.add(new Real(1.00));
-				System.out.println("Uma moeda de 1 Real foi foi removida do Cofrim!");
-				break;
-			default:
-				System.out.println("Opção inválida!");
-			}
-		case 2:
-			switch(escolhaValor) {
-			case 1:
-				moedas.add(new Dolar(0.10));
-				System.out.println("Uma moeda de 10 centavos (Dólar) foi removida do Cofrim!");
-				break;
-			case 2:
-				moedas.add(new Dolar(0.25));
-				System.out.println("Uma moeda de 25 centavos (Dólar) foi removida do Cofrim!");
-				break;
-			case 3:
-				moedas.add(new Dolar(0.50));
-				System.out.println("Uma moeda de 50 centavos (Dólar) foi removida do Cofrim!");
-				break;
-			case 4:
-				moedas.add(new Dolar(1.00));
-				System.out.println("Uma moeda de 1 Dólar foi removida do Cofrim!");
-				break;
-			default:
-				System.out.println("Opção inválida!");
-			}
-		case 3:
-			switch(escolhaValor) {
-			case 1:
-				moedas.add(new Euro(0.10));
-				System.out.println("Uma moeda de 10 centavos (Euro) foi removida do Cofrim!");
-				break;
-			case 2:
-				moedas.add(new Euro(0.25));
-				System.out.println("Uma moeda de 25 centavos (Euro) foi removida do Cofrim!");
-				break;
-			case 3:
-				moedas.add(new Euro(0.50));
-				System.out.println("Uma moeda de 50 centavos (Euro) foi removida do Cofrim!");
-				break;
-			case 4:
-				moedas.add(new Euro(1.00));
-				System.out.println("Uma moeda de 1 Euro foi removida do Cofrim!");
-				break;
-			default:
-				System.out.println("Opção inválida!");
-			}
+	public void remover(int escolhaMoeda) {
+		try {
+			cofrinho.remove(escolhaMoeda-1);
+			System.out.println("Moeda removida do Cofrim com sucesso!");
+		}catch(Exception e) {
+			System.out.println("Ocorreu um erro ao remover a moeda!" + e);
 		}
 	}
 }
--- cofrinhopoo/Main.java
+++ cofrinhopoo/Main.java
@@ -3,6 +3,7 @@ package cofrinhopoo;
 //OBS: Terminar de configurar case 3 e 4 e verificar se 1 e 2 precisam de mais alguma alteração
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -53,35 +54,35 @@ public class Main {
 				break;
 			case 2://Escolhe moeda, valor e a remove do cofrinho (classe Cofrinho)
 				escolhaMoeda = 0;
+				int numMoeda = 1;
 				try {
-					while(escolhaMoeda > 3 || escolhaMoeda <= 0) {
-						Menu.escolhaRemover();
-						escolhaMoeda = inputUsuario.nextInt();
-						if(escolhaMoeda!=1 && escolhaMoeda!=2 && escolhaMoeda!=3) {
-							System.out.println("Opção inválida!");
-							break;
-						}
+					System.out.println("Estas são suas moedas:");
+					while(cofrinho.it.hasNext()) {
+						Moeda moeda = cofrinho.it.next();
+						System.out.printf("%d - %s: Moeda de %f", numMoeda, moeda.nome, moeda.valor);
+						numMoeda++;
 					}
-					System.out.println("Qual valor gostaria de remover?");
-					escolhaValor = inputUsuario.nextInt();
-					if(escolhaValor < 0) {
-						System.out.println("Opa! Digite apenas o valor 'limpo', sem utilizar números negativos!");
+					escolhaMoeda = inputUsuario.nextInt();
+					if(escolhaMoeda<1 || escolhaMoeda > cofrinho.cofrinho.size()) {
+						System.out.println("Opção inválida!");
 						break;
 					}
-					cofrinho.remover(escolhaMoeda, escolhaValor);
+					cofrinho.remover(escolhaMoeda);
 				}catch(Exception erro) {
 					System.out.println("Opa, algo deu errado! Você digitou o número da opção correta?\n" + erro);
 				}
 				break;
 			case 3://Faz a iteração para mostrar o valor de cada moeda guardada
-				System.out.println("Aqui estão suas moedas:\n");
-				
-				for(Moeda moeda: cofrinho.moedas) {
-					if(moeda.valor == 0) {
-						System.out.println(moeda.nome + ": Vazio! :( \n");
-					}else{
-						System.out.println(moeda.nome + ": " + moeda.valor + "\n");
+				numMoeda = 1;
+				try {
+					System.out.println("Estas são suas moedas:");
+					while(cofrinho.it.hasNext()) {
+						Moeda moeda = cofrinho.it.next();
+						System.out.printf("%d - %s: Moeda de %f", numMoeda, moeda.nome, moeda.valor);
+						numMoeda++;
 					}
+				}catch(Exception erro) {
+					System.out.println("Opa, algo deu errado! Você digitou o número da opção correta?\n" + erro);
 				}
 				break;
 			case 4://Converte tudo e mostra o valor em reais
